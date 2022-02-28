0c357499c4cc5a5e915d780e7527502b7c753d33
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-03-01 02:43:44 +0600

    half is done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 console.log('allah')
+// main div 
+const mainDiv = document.getElementById('main-div')
+// api load 
+const loadApi = () => {
+    const inputValue = document.getElementById('input-fild').value;
+    fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
+        .then((res) => res.json())
+        .then(data => displayPhoneCard(data))
+}
+// display phone info in Card
+const displayPhoneCard = (data) => {
+    // console.log(data)
+    const phonearray = data.data;
+    console.log(phonearray);
+    phonearray.forEach(info => {
+        console.log(info)
+        const childDiv = document.createElement('div');
+        childDiv.classList.add('col');
+        childDiv.innerHTML = `
+        <div id="card-div" class="card mx-auto h-100 w-75">
+          <img src="${info.image}" class="card-img-top h-75 w-100" alt="...">
+          <div class="card-body">
+            <h3 class="card-title">${info.phone_name}</h3>
+            <h3 class="card-title">${info.brand}</h3>
+            <button type="button" id="button-details" class="btn">Details</button>
+          </div>
+        </div>`;
+        mainDiv.appendChild(childDiv);
+    });
+
+}
