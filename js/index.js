2b9b08a479820c0129e7b2c6d21b741f95e2bd67
md-adnan-ahsan <syed.jaid.7748@gmail.com>
2022-03-01 20:47:03 +0600

    all commit is done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 console.log('allah')
-// main div 
+// main div of card and details
 const mainDiv = document.getElementById('main-div')
 const mainDetails = document.getElementById('details-div')
 // api load 
@@ -11,17 +11,20 @@ const loadApi = () => {
         .then((res) => res.json())
         .then(data => displayPhoneCard(data))
 }
-
 // display phone info in Card
 const displayPhoneCard = (data) => {
     const datacall = data.status;
     const phonearray = data.data;
+    // if input Value is correct
     if (datacall == true) {
+        // using foreach to get all inner data of array 
         phonearray.slice(0, 20).forEach(info => {
+            // div create 
             const childDiv = document.createElement('div');
             childDiv.classList.add('col');
+            // div inner HTML
             childDiv.innerHTML = `
-        <div id="card-div" class="card  mx-auto h-100 w-75">
+        <div id="card-div" class="card p-2 shadow rounded-3 mx-auto h-100 w-75">
           <img src="${info.image}" class=" card-img-top h-75 w-100" alt="...">
           <div class="card-body">
             <h4 class="card-title"><span class="fw-bold">Name : </span> ${info.phone_name}</h4>
@@ -29,9 +32,11 @@ const displayPhoneCard = (data) => {
             <button type="button" onclick="phoneDetails('${info.slug}')" id="button-details" class="btn">Details</button>
           </div>
         </div>`;
+            // appending  Child to the main div 
             mainDiv.appendChild(childDiv);
         });
     }
+    // if input Value is not correct
     else {
         const mainDetails = document.getElementById('main-div');
         const childh1 = document.createElement('h1');
@@ -39,30 +44,37 @@ const displayPhoneCard = (data) => {
         childh1.innerHTML = `Sorry no phone found`
         mainDetails.appendChild(childh1);
     }
+    // show all button  to the div
     const buttonDiv = document.createElement('div')
-    buttonDiv.innerHTML = `<button onclick="displayall()">Show all</button>`
+    buttonDiv.innerHTML = `
+    <button type="button" onclick="displayall()" class="btn btn-info ps-3 pe-3 mx-auto d-block">Show all</button>`
+    // appendChild condtion 
     if (datacall == true) {
         mainDiv.appendChild(buttonDiv)
     }
 
 }
 const phoneDetails = (id) => {
-    console.log(id);
+    //    loading data of phone details 
     fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
         .then(res => res.json())
         .then(data => displayDetails(data))
 }
-
+// display Details to the Header 
 const displayDetails = (data) => {
     mainDetails.innerHTML = '';
     const phoneInfo = data.data;
+    // creating the div 
     const childDiv2 = document.createElement('div');
+    console.log(phoneInfo.mainFeatures.displaySize);
+    // if release date is empty string 
     if (phoneInfo.releaseDate == '') {
+        // seting inner HTML to the details div     
         childDiv2.innerHTML = `
-    <div>
-    <img src="${phoneInfo.image}" class="card-img-top" alt="...">
+    <div class="border border-white">
+        <div>
+    <img src="${phoneInfo.image}" class="card-img-top p-2" alt="...">
     </div>
-
     <div class="card-body">
     <ul class="list-group list-group-flush">
         <li class="list-group-item"><span class="fw-bold fs-5">Name : </span>${phoneInfo.name}</li>
@@ -73,6 +85,9 @@ const displayDetails = (data) => {
             <br>
             <span><span class="fw-bold">storage:</span>${phoneInfo.mainFeatures.storage}</span>
             <br>
+            <span><span class="fw-bold">displaySize:</span>
+            ${phoneInfo.mainFeatures.displaySize}</span>
+            <br>
             <span><span class="fw-bold">chipSet:</span>${phoneInfo.mainFeatures.chipSet}</span>
             <br>
             <span><span class="fw-bold">memory:</span>${phoneInfo.mainFeatures.memory}</span>
@@ -84,47 +99,53 @@ const displayDetails = (data) => {
             ${phoneInfo.mainFeatures.sensors}</span>
         </li>
     </ul>
-    </div>`
-        mainDetails.appendChild(childDiv2);
-
-    }
-    else {
-        childDiv2.innerHTML = `
-    <div>
-    <img src="${phoneInfo.image}" class="card-img-top" alt="...">
     </div>
+    </div>`
+        mainDetails.appendChild(childDiv2);
 
-    <div class="card-body">
-    <ul class="list-group list-group-flush">
-        <li class="list-group-item"><span class="fw-bold fs-5">Name : </span>${phoneInfo.name}</li>
-        <li class="list-group-item"><span class="fw-bold fs-5">Brand : </span>${phoneInfo.brand}</li>
-        <li class="list-group-item"><span class="fw-bold fs-5">releaseDate : </span>${phoneInfo.releaseDate}</li>
-        <li class="list-group-item">
-            <span class="fs-5 fw-bold">mainFeatures</span>
-            <br>
-            <span><span class="fw-bold">storage:</span>${phoneInfo.mainFeatures.storage}</span>
-            <br>
-            <span><span class="fw-bold">chipSet:</span>${phoneInfo.mainFeatures.chipSet}</span>
-            <br>
-            <span><span class="fw-bold">memory:</span>${phoneInfo.mainFeatures.memory}</span>
-        </li>
-        <li class="list-group-item">
-            <span class="fs-5 fw-bold">Sensors:</span>
-            <br>
-            <span><span class="fw-bold"></span>
-            ${phoneInfo.mainFeatures.sensors}</span>
-        </li>
-    </ul>
+    }
+    // if release date is string 
+    else {
+        // seting inner HTML to the details div   
+        childDiv2.innerHTML = `
+    <div class="border border-white">
+            <div>
+            <img src="${phoneInfo.image}" class="card-img-top p-2" alt="...">
+            </div>
+
+            <div class="card-body">
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item"><span class="fw-bold fs-5">Name : </span>${phoneInfo.name}</li>
+                <li class="list-group-item"><span class="fw-bold fs-5">Brand : </span>${phoneInfo.brand}</li>
+                <li class="list-group-item"><span class="fw-bold fs-5">releaseDate : </span>${phoneInfo.releaseDate}</li>
+                <li class="list-group-item">
+                <span class="fs-5 fw-bold">mainFeatures</span>
+                <br>
+                <span><span class="fw-bold">storage:</span>${phoneInfo.mainFeatures.storage}</span>
+                <br>
+                <span><span class="fw-bold">displaySize:</span>
+                ${phoneInfo.mainFeatures.displaySize}</span>
+                <span><span class="fw-bold">chipSet:</span>${phoneInfo.mainFeatures.chipSet}</span>
+                <br>
+                <span><span class="fw-bold">memory:</span>${phoneInfo.mainFeatures.memory}</span>
+            </li>
+            <li class="list-group-item">
+                <span class="fs-5 fw-bold">Sensors:</span>
+                <br>
+                <span><span class="fw-bold"></span>
+                ${phoneInfo.mainFeatures.sensors}</span>
+            </li>
+        </ul>
+        </div>
     </div>`
         mainDetails.appendChild(childDiv2);
     }
-
-
 }
+// showing all phone to the div 
 const displayall = () => {
-
     mainDiv.innerHTML = '';
     mainDetails.innerHTML = '';
+    // loding phone data 
     const inputValue = document.getElementById('input-fild').value;
     fetch(`https://openapi.programming-hero.com/api/phones?search=${inputValue}`)
         .then((res) => res.json())
@@ -133,6 +154,7 @@ const displayall = () => {
 // display phone info in Card
 const displayallCard = (data) => {
     const phonearray = data.data;
+    // using foreach to get all inner data of array  
     phonearray.forEach(info => {
         const childDiv = document.createElement('div');
         childDiv.classList.add('col');
